47e34d1d64da9286683aa2f1fd3ef07526a98b44
Jakob Steimle <jakob.steimle@tum.de>
2023-06-23 13:05:15 +0200

    Set initial server round to 0

--- seleceval/util/config.py
+++ seleceval/util/config.py
@@ -3,7 +3,7 @@ import json
 
 class Config:
     def __init__(self, file_name: str):
-        self.current_round = 1
+        self.current_round = 0
         with open(file_name) as json_file:
             self.initial_config = json.load(json_file)
 
