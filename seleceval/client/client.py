3afff9d08f7dab6fc03308456bc63797cc34bb77
Jakob Steimle <jakob.steimle@tum.de>
2023-07-16 11:10:31 +0200

    Improved cleint

--- seleceval/client/client.py
+++ seleceval/client/client.py
@@ -99,7 +99,7 @@ class Client(fl.client.NumPyClient):
                 "network_bandwidth": self.state.get('network_bandwidth'),
                 "client_name": self.state.get('client_name'),
                 "expected_execution_time": self.state.get('expected_execution_time'),
-                "sample_size": 500
+                "sample_size": len(self.trainloader)
                 }
 
     def _calculate_timeout(self) -> bool:
