65b6cd8232235950a42cb9e17c983ac0291cb718
Jakob Steimle <jakob.steimle@tum.de>
2023-11-13 12:32:08 +0100

    Minor improvements to output. Updated client generation to use numpy

--- seleceval/simulation/state.py
+++ seleceval/simulation/state.py
@@ -5,6 +5,7 @@ import random
 
 import pandas as pd
 import randomname
+import numpy as np
 
 from ..util import Config
 
@@ -19,84 +20,28 @@ def generate_initial_state(num_clients: int, config: Config):
     client_configurations = pd.read_csv(
         config.initial_config["client_configuration_file"]
     ).to_dict("records")
-    for i in range(num_clients):
-        performance_tier = random.randint(
-            0,
-            config.initial_config["simulation_config"]["number_of_performance_tiers"]
-            - 1,
-        )
-        cpu = client_configurations[performance_tier]["cpu"]
-        ram = client_configurations[performance_tier]["ram"]
-        expected_execution_time = client_configurations[performance_tier][
-            "expected_execution_time"
-        ]
-        network_bandwidth = get_network_bandwidth(config)
-        client_name = randomname.get_name()
-        reliability = get_reliability(config)
+    df = pd.DataFrame()
+    df['performance_tier'] = np.random.randint(0,
+                                               config.initial_config["simulation_config"][
+                                                   "number_of_performance_tiers"] - 1,
+                                               num_clients)
+    df['network_bandwidth'] = np.round(np.maximum(
+        np.random.normal(config.initial_config["simulation_config"]["network_bandwidth_mean"],
+                         config.initial_config["simulation_config"]["network_bandwidth_std"], num_clients),
+        np.zeros(num_clients)), 2)
+    df['client_name'] = list(randomname.sample_names(num_clients))
+    df['i_reliability'] = np.round(
+        np.random.exponential(1 / config.initial_config["simulation_config"]["reliability_parameter"],
+                              num_clients), 5)
+    df['performance_factor'] = np.round(
+        np.random.normal(config.initial_config["simulation_config"]["performance_factor_mean"],
+                         config.initial_config["simulation_config"]["performance_factor_std"], num_clients), 2)
 
-        performance_factor = get_performance_factor(config)
-        records.append(
-            [
-                cpu,
-                ram,
-                network_bandwidth,
-                reliability,
-                performance_tier,
-                expected_execution_time,
-                performance_factor,
-                client_name,
-            ]
-        )
-    df = pd.DataFrame(
-        records,
-        columns=[
-            "cpu",
-            "ram",
-            "network_bandwidth",
-            "i_reliability",
-            "performance_tier",
-            "expected_execution_time",
-            "i_performance_factor",
-            "client_name",
-        ],
-    )
+    df['cpu'] = df['performance_tier'].apply(lambda x: client_configurations[x]["cpu"])
+    df['ram'] = df['performance_tier'].apply(lambda x: client_configurations[x]["ram"])
+    df['expected_execution_time'] = df['performance_tier'].apply(
+        lambda x: client_configurations[x]["expected_execution_time"])
     df.to_csv(config.attributes["input_state_file"], index=False)
-
-
-def get_network_bandwidth(config):
-    return max(
-        [
-            round(
-                random.gauss(
-                    config.initial_config["simulation_config"][
-                        "network_bandwidth_mean"
-                    ],
-                    config.initial_config["simulation_config"]["network_bandwidth_std"],
-                ),
-                2,
-            ),
-            config.initial_config["simulation_config"]["network_bandwidth_min"],
-        ]
-    )
-
-
-def get_performance_factor(config):
-    return round(
-        random.gauss(
-            config.initial_config["simulation_config"]["performance_factor_mean"],
-            config.initial_config["simulation_config"]["performance_factor_std"],
-        ),
-        2,
-    )
-
-
-def get_reliability(config):
-    return round(
-        random.expovariate(
-            config.initial_config["simulation_config"]["reliability_parameter"]
-        ),
-        5,
-    )
 
 
 def get_initial_state(num_clients: int, config: Config):
@@ -131,12 +76,13 @@ def run_state_update(config: Config, server_round: int):
         + server_round
     )
     state_df = pd.read_csv(config.attributes["working_state_file"])
-    state_df["network_bandwidth"] = state_df["network_bandwidth"].transform(
-        lambda x: get_network_bandwidth(config)
-    )
-    state_df["i_performance_factor"] = state_df["i_performance_factor"].transform(
-        lambda x: get_performance_factor(config)
-    )
+    state_df["network_bandwidth"] = np.round(np.maximum(
+        np.random.normal(config.initial_config["simulation_config"]["network_bandwidth_mean"],
+                         config.initial_config["simulation_config"]["network_bandwidth_std"], len(state_df.index)),
+        np.zeros(len(state_df.index))), 2)
+    state_df["i_performance_factor"] = np.round(
+        np.random.normal(config.initial_config["simulation_config"]["performance_factor_mean"],
+                         config.initial_config["simulation_config"]["performance_factor_std"], len(state_df.index)), 2)
     state_df.to_csv(config.attributes["working_state_file"], index=False)
     state_df.to_csv(
         config.attributes["state_output_prefix"] + str(server_round) + ".csv",
