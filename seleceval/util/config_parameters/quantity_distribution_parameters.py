b051d6e796b25749959fe3cb2b6230fddf27dd7f
Tim Würfl <tim.wuerfl@tum.de>
2024-02-01 12:10:04 +0100

    changed config

--- seleceval/util/config_parameters/quantity_distribution_parameters.py
+++ seleceval/util/config_parameters/quantity_distribution_parameters.py
@@ -13,8 +13,8 @@ data_quantity_distribution_parameters = {
     },
     "data_quantity_skew_parameter_1": {"type": "float", "min": 0, "default": 0.5},
     "data_quantity_skew_parameter_2": {"type": "float", "min": 0, "default": 0.5},
-    "data_quantity_min_parameter": {"type": "integer", "min": 0, "default": 1},
+    "data_quantity_min_parameter": {"type": "integer", "min": 0, "default": 32},
     "data_quantity_max_parameter": {"type": "integer", "min": 1, "default": 2000}
 }
 
-default_quantity_distribution = "Uniform"
+default_quantity_distribution = "Dirichlet"
