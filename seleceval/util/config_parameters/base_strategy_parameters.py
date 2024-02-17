4d22c36a553836e9aa8eaeb4c3aa8fcc0b285cbe
Tim Würfl <tim.wuerfl@tum.de>
2024-02-17 14:45:17 +0100

    changed config, Test of FedAvg with equal weights, comparison with Disco equal weights and Nova (Fedavg?) and FedProxmu=0 aka FedAvg, changed to no momentum for all BS since not clear of inter-round effect

--- seleceval/util/config_parameters/base_strategy_parameters.py
+++ seleceval/util/config_parameters/base_strategy_parameters.py
@@ -11,16 +11,16 @@ base_strategy_parameter_dict = {
         "default": {},
         "schema": {
             "lr": {"type": "float", "min": 0, "max": 1, "default": 0.01},
-            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.9}
+            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.0}
         },
     },
     "FedProx": {
         "type": "dict",
         "default": {},
         "schema": {
-            "mu": {"type": "float", "min": 0, "max": 1, "default": 0.01},
+            "mu": {"type": "float", "min": 0, "max": 1, "default": 0.0},
             "lr": {"type": "float", "min": 0, "max": 1, "default": 0.01},
-            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.9}
+            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.0}
         },
     },
     "FedNova": {
@@ -30,7 +30,7 @@ base_strategy_parameter_dict = {
             "lr": {"type": "float", "min": 0, "max": 1, "default": 0.01},
             "gmf": {"type": "float", "min": 0, "max": 1, "default": 0.0},
             "mu": {"type": "float", "min": 0, "max": 1, "default": 0.0},
-            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.9}
+            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.0}
         },
     },
     "FedDisco": {
@@ -39,7 +39,7 @@ base_strategy_parameter_dict = {
         "schema": {
             "lr": {"type": "float", "min": 0, "max": 1, "default": 0.01},
             "mu": {"type": "float", "min": 0, "max": 1, "default": 0.0},
-            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.9},
+            "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.0},
             "a": {"type": "float", "min": 0, "max": 2, "default": 2},
             "b": {"type": "float", "min": 0, "max": 1, "default": 0.1}
 
@@ -51,7 +51,7 @@ base_strategy_parameter_dict = {
                 "schema": {
                     "lr": {"type": "float", "min": 0, "max": 1, "default": 0.01},
                     "gmf": {"type": "float", "min": 0, "max": 1, "default": 0.5},
-                    "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.9}
+                    "momentum": {"type": "float", "min": 0, "max": 1, "default": 0.0}
                 }
     }
 }
